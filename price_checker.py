fa77e6105c5de89ce8a0ada7f5e2746e4e28c913
ljllacuna4 <qweqwe123.aa690@gmail.com>
2020-08-21 16:58:20 +0100

    Added comments, found to product_id

--- price_checker.py
+++ price_checker.py
@@ -9,29 +9,36 @@ headers = { "User-Agent" : "Mozilla/5.0" }
 
 
 def get_product_id(amazon_url):
+    #Returns the product id from the amazon URL
     try:
-        found = re.search('product/(.+?)/', amazon_url).group(1)
+        product_id = re.search('product/(.+?)/', amazon_url).group(1)
     except AttributeError:
-        found = amazon_url
-    return found
+        product_id = amazon_url  #Returns amazon URL if the inp is already the product ID
+    return product_id
 
 
 def get_price(amazon_url):
+    """This function takes an amazon product url and returns its current price"""
 
-    found = get_product_id(amazon_url)
+    #product id is extracted form amazon url
+    product_id = get_product_id(amazon_url)
 
-    URL = f'https://uk.camelcamelcamel.com/product/{found}'
+    #product id is added to the camel site
+    URL = f'https://uk.camelcamelcamel.com/product/{product_id}'
+    
+    #request for the webpage content
     response = requests.get(URL, headers = headers)
+    #parse webpage content
     soup = BeautifulSoup(response.content, "html.parser")  
+    #find amazon price table
     price_table = soup.find("table" , class_="product_pane")
+    #return current price
     price = price_table.find_all("td")[1]
 
     return price.text
 
-def to_table(l, n):
-    return [l[i:i+n] for i in range(0, len(l), n)]
 
+amazon_url = input("Enter amazon link: ")   #input amazon url
+price = get_price(amazon_url)   #get its current price
 
-amazon_url = input("Enter amazon link: ")
-price = get_price(amazon_url)
-print(price)
+print(price)    
